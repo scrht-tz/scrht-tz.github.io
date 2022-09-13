4a997b10ae2a6af3fe5745e7a0e957950354f5ed
TioZucaBR <scrht.tz@gmail.com>
2022-09-13 16:45:34 -0300

    .

--- login/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:secret@localhost/flask_login'
-
-login_manager = LoginManager(app)
-db = SQLAlchemy(app)
