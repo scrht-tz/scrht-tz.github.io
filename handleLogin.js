fbffd4af0dd5c01d37a20bf14fa7aaf35b161d3c
TioZucaBR <scrht.tz@gmail.com>
2022-09-20 14:24:06 -0300

    organização das tarefas (tá péssimo)

--- handleLogin.js
+++ handleLogin.js
@@ -1,25 +1,40 @@
-async function getJSON(url){
-    let response = await fetch(url)
-    let data = await response.json()
-    return data
-}
-
-const usersJSON = getJSON('users.json')
-console.log(usersJSON)
-
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop)
 })
 
-function User(username, email, password) {
-    this.username = username
-    this.email = email
-    this.password = password
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+    checkPassword(password, realPassword) {
+        console.log(`[user ${this.username}] Checking password...`)
+        if (password === realPassword){
+            console.log(`[user ${this.username}] Password is real!`)
+            return true
+        }
+        else{
+            console.log(`[user ${this.username}] Password isn't real.`)
+            return false
+        }
+    }
 }
 
 if (params.email && params.password){
-    const user = new User(params.username, params.email, params.password)
-    console.log(user)
+    if (params.submit === 'Entrar'){
+        var user = new User(params.username, params.email, params.password)
+        fetch('users.json').then(response => {
+            return response.json()
+        }).then(
+            data => {
+                for (user in data.users){
+                    if (user.checkPassword(params.password, user.password))
+                        console.log(user)
+                }
+            }
+        )
+    }
 }
 else if (!params.email && !params.password && params.submit === 'Entrar') {
     window.alert('Você não logou.')
--- tarefas-objetivo/script.js
+++ tarefas-objetivo/script.js
@@ -1,67 +1,51 @@
-const tarefasdiv = document.getElementById('tarefas')
-
-async function update(url){
-    const response = await fetch(url)
+async function update(){
+    console.clear()
+    const response = await fetch('data/tarefas.json')
     const tarefas = await response.json()
-    for (let i = 0; i < tarefas.terceirobimestre.length; i++) {
-        if (document.getElementById(tarefas.terceirobimestre[i].data)){
-            console.log(`A data ${tarefas.terceirobimestre[i].data} já foi inserida`)
-            const titleDiv = document.createElement('h3')
-            const materiadiv = document.createElement('div')
-            const materiatitle = document.createElement('h4')
-            const tarefatarefa = document.createElement('p')
-            const dataentrega = document.createElement('p')
-            if (tarefas.terceirobimestre[i].materia === 'Matemática Aplicada'){
-                materiadiv.id = 'matplicada'}
-            else if (tarefas.terceirobimestre[i].materia === 'Gramática'){
-                materiadiv.id = 'gramatica'
-            }
-            else if (tarefas.terceirobimestre[i].materia == 'Literatura'){
-                materiadiv.id = 'literatura'
-            }
-            else if (tarefas.terceirobimestre[i].materia == 'Ciências'){
-                materiadiv.id = 'ciencias'
-            }
-            else if (tarefas.terceirobimestre[i].materia == 'História'){
-                materiadiv.id = 'historia'
-            }
-            titleDiv.id = 'data'
-            titleDiv.textContent = tarefas.terceirobimestre[i].data
-            dataentrega.textContent = `Data de Entrega: ${tarefas.terceirobimestre[i].dataentrega}`
-            dataentrega.id = 'dataentrega'
-            materiatitle.id = 'titulotarefa'
-            materiatitle.textContent = tarefas.terceirobimestre[i].materia
-            tarefatarefa.textContent = tarefas.terceirobimestre[i].tarefa
-            tarefatarefa.id = 'tarefatarefa'
-            materiadiv.appendChild(materiatitle)
-            materiadiv.appendChild(tarefatarefa)
-            materiadiv.appendChild(dataentrega)
-            document.getElementById(tarefas.terceirobimestre[i].data).appendChild(materiadiv)
-        }
-        else {
+
+    if(document.getElementById('tarefas')){
+        document.body.removeChild(document.getElementById('tarefas'))
+    }
+    else {
+        console.log(document.getElementById('tarefas'))
+    }
+    var tarefasdiv = document.createElement('div')
+    tarefasdiv.id = 'tarefas'
+    document.body.insertAdjacentElement('beforeend', tarefasdiv)
+
+    setTimeout(function () {
+        for (let i = 0; i < tarefas.terceirobimestre.length; i++) {
             const newDiv = document.createElement('div')
             const titleDiv = document.createElement('h3')
             const materiadiv = document.createElement('div')
             const materiatitle = document.createElement('h4')
             const tarefatarefa = document.createElement('p')
             const dataentrega = document.createElement('p')
-            if (tarefas.terceirobimestre[i].materia === 'Matemática Aplicada'){
-                materiadiv.id = 'matplicada'}
-            else if (tarefas.terceirobimestre[i].materia === 'Gramática'){
+            var year = tarefas.terceirobimestre[i].data.split('/')[2]
+            var month = tarefas.terceirobimestre[i].data.split('/')[1]
+            var day = tarefas.terceirobimestre[i].data.split('/')[0]
+            const data = new Date(year, month, day)
+            if (tarefas.terceirobimestre[i].materia === 'Matemática Aplicada') {
+                materiadiv.id = 'matplicada'
+            }
+            else if (tarefas.terceirobimestre[i].materia === 'Gramática') {
                 materiadiv.id = 'gramatica'
             }
-            else if (tarefas.terceirobimestre[i].materia === 'Literatura'){
+            else if (tarefas.terceirobimestre[i].materia === 'Literatura') {
                 materiadiv.id = 'literatura'
             }
-            else if (tarefas.terceirobimestre[i].materia === 'Ciências'){
+            else if (tarefas.terceirobimestre[i].materia === 'Ciências') {
                 materiadiv.id = 'ciencias'
             }
-            else if (tarefas.terceirobimestre[i].materia === "Matemática"){
+            else if (tarefas.terceirobimestre[i].materia === "Matemática") {
                 materiadiv.id = 'matematica'
+            }
+            else if (tarefas.terceirobimestre[i].materia === 'História') {
+                materiadiv.id = 'historia'
             }
             titleDiv.id = 'data'
             titleDiv.textContent = tarefas.terceirobimestre[i].data
-            dataentrega.textContent = tarefas.terceirobimestre[i].dataentrega
+            dataentrega.textContent = `Data de Entrega: ${tarefas.terceirobimestre[i].dataentrega}`
             dataentrega.id = 'dataentrega'
             materiatitle.id = 'titulotarefa'
             materiatitle.textContent = tarefas.terceirobimestre[i].materia
@@ -73,15 +57,36 @@ async function update(url){
             materiadiv.appendChild(tarefatarefa)
             materiadiv.appendChild(dataentrega)
             newDiv.appendChild(materiadiv)
-            console.log(tarefasdiv.childNodes)
-            if (newDiv.id.split('/')[1] > tarefasdiv.firstElementChild.id.split('/')[1] || newDiv.id.split('/')[0] > tarefasdiv.firstElementChild.id.split('/')[0]){
-                tarefasdiv.insertAdjacentElement('afterbegin', newDiv)
+            if (document.getElementById(tarefas.terceirobimestre[i].data)) {
+                console.log(`A data ${tarefas.terceirobimestre[i].data} já foi inserida`)
+                document.getElementById(tarefas.terceirobimestre[i].data).appendChild(materiadiv)
             }
-            else if (newDiv.id.split('/')[1] < tarefasdiv.lastElementChild.id.split('/')[1] || newDiv.id.split('/')[0] < tarefasdiv.lastElementChild.id.split('/')[0]){
-                tarefasdiv.insertAdjacentElement('beforeend', newDiv)
+            else {
+                if (i === 0) {
+                    tarefasdiv.appendChild(newDiv)
+                }
+                else if (data.getMonth() > tarefasdiv.firstElementChild.id.split('/')[1] || data.getDay() > tarefasdiv.firstElementChild.id.split('/')[0]) {
+                    tarefasdiv.insertAdjacentElement('afterbegin', newDiv)
+                }
+                else if (data.getMonth() < tarefasdiv.firstElementChild.id.split('/')[1] || data.getDay() < tarefasdiv.firstElementChild.id.split('/')[0]) {
+                    tarefasdiv.insertBefore(newDiv, tarefasdiv.firstElementChild)
+                }
+                else {
+                    tarefasdiv.insertAdjacentElement('beforeend', newDiv)
+                }
             }
         }
+    }, 10)
+    setTimeout(organize, 10)
+}
+
+async function organize(){
+    var tarefasdiv = document.getElementById('tarefas')
+
+    for (let i = tarefasdiv.length-1; i >= 0; i--){
+        console.log(tarefasdiv.childNodes[i])
     }
 }
 
-update('data/tarefas.json')
+update()
+document.getElementById('reloadbutton').addEventListener('click', update)
